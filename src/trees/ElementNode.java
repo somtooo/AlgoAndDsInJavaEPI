d279f8d7923af6a39118d2548efe9d2d884bc08b
somtooo <chukwurahb@gmail.com>
2021-09-01 09:24:29 -0400

    Solved tree question

--- src/trees/ElementNode.java
+++ src/trees/ElementNode.java
@@ -1,5 +1,7 @@
 package trees;
 
+import java.util.List;
+
 public class ElementNode<T extends Comparable<T>> {
     private final T object;
     private ElementNode<T> rightNode;
@@ -21,9 +23,9 @@ public class ElementNode<T extends Comparable<T>> {
         return this;
     }
 
-    public int height(ElementNode<T> tree) {
+    public Integer height(ElementNode<T> tree) {
         if (tree == null) {
-            return 0;
+            return null;
         }
         int left = (tree.leftNode!=null)? tree.height(tree.leftNode)+1 :0;
         int right = (tree.rightNode!=null) ? tree.rightNode.height(tree.rightNode) + 1 : 0;
@@ -38,40 +40,56 @@ public class ElementNode<T extends Comparable<T>> {
         if (right.length() == 0) {
             return object;
         }
-        return object + " " + right;
+        return object +  right;
+    }
+
+    public String preOrderRight() {
+        var object = this.object.toString();
+        var right = (this.rightNode!=null) ? this.rightNode.preOrderRight() : "";
+        object = object + " " + right;
+        var left = (this.leftNode!=null) ? this.leftNode.preOrderRight(): "";
+        if (left.length() == 0) {
+            return object;
+        }
+        return object  + left;
     }
 
     public boolean heightBalanced(ElementNode<T> root) {
+
         if (root == null) {
             return true;
         }
-        var left = (height(root.leftNode) - height(root.rightNode)) <= 1 && heightBalanced(root.leftNode);
+        var leftHieght = height(root.leftNode);
+        var rightHieght = height(root.rightNode);
+        if (leftHieght == null | rightHieght == null){
+            return false;
+        }
+        var left = (leftHieght- rightHieght) <= 1 ? heightBalanced(root.leftNode) : false;
         if (!left) {
             return false;
         }
         var right = heightBalanced(root.rightNode);
         return left&right;
     }
+
+
+
+
+    public boolean symmetricalTree(ElementNode<T> root) {
+
+        return root.leftNode.preOrderRight().equals(root.rightNode.preOrder());
+    }
+
+
     public static void main(String[] args) {
-        var root = new ElementNode<Integer>(20);
-        root.add(40);
-        root.add(30);
-        root.add(45);
+        var root = new ElementNode<Integer>(10);
+        root.add(20);
         root.add(25);
-        root.add(35);
-
         root.add(15);
-        root.add(17);
-
-        root.add(19);
-        root.add(16);
-        root.add(11);
-        root.add(14);
         root.add(8);
+        root.add(6);
         root.add(4);
-        root.add(9);
 
-
-        System.out.println(root.heightBalanced(root));
+        System.out.println(root.preOrderRight());
     }
 }
